e6820ed7224deee3b1863fb29ab6b3be2047d245
Вера <verazverev@yandex.ru>
2020-01-24 20:46:48 +0500

    1

--- 5.py
+++ 5.py
@@ -4,13 +4,21 @@ def new_func(*args):
         if el != "#":
             result = result + int(el)
         else:
-            print(result)
+            print(result + args[1])
             return ("#")
-    print(result)
+    return(result)
 
 
 string = ' '
+string2=0
 print('Для окончания работы программы введите значение "#"')
 while string != "#":
     string = input("Введите данные через пробел: ")
-    string = new_func(string.split())
+    if ( new_func(string.split() , string2))!="#":
+        string2=  string2+ new_func(string.split(), string2)
+        print(string2)
+    else:
+        break
+
+
+
